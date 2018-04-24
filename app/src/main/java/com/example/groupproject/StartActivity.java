45a4911301b55b579a38c3afa262cdec08af73fd
hellofrankxp <xiaopenli4-c@my.cityu.edu.hk>
2018-04-24 10:27:18 +0800

    合并引导界面

---
+++ app/src/main/java/com/example/groupproject/StartActivity.java
@@ -0,0 +1,71 @@
+package com.example.groupproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+
+public class StartActivity extends Activity {
+    SharedPreferences preferences;
+    private ImageView welcomeImg = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start);
+        // 读取SharedPreferences中需要的数据
+        preferences = getSharedPreferences("count", MODE_PRIVATE);
+        int count = preferences.getInt("count", 0);
+        // 判断程序与第几次运行，如果是第一次运行则跳转到引导页面
+        if (count == 0) {
+            Intent intent = new Intent();
+            intent.setClass(getApplicationContext(), GuideActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        Editor editor = preferences.edit();
+        // 存入数据
+        editor.putInt("count", ++count);
+        // 提交修改
+        editor.commit();
+
+        welcomeImg = (ImageView) this.findViewById(R.id.welcome_img);
+        AlphaAnimation anima = new AlphaAnimation(0.3f, 1.0f);
+        anima.setDuration(4000);// 设置动画显示时间
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new AnimationImpl());
+    }
+
+    private class AnimationImpl implements AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+            welcomeImg.setBackgroundResource(R.drawable.startpage);
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            skip(); // 动画结束后跳转到别的页面
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
+    }
+
+    private void skip() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
